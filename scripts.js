540d865d7cc7da7793e98053b1809dfbccddef68
Adam Dunmore <94834543+AdamDunmore@users.noreply.github.com>
2023-04-05 16:53:06 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,21 +11,7 @@ function witch(){
 
 class main{
     constructor(){
-      
-      <!-- Insert this script at the bottom of the HTML, but before you use any Firebase services -->
-      import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
-
-      // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-      import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js'
-
-      // Add Firebase products that you want to use
-      import { getAuth } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
-      import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
-
-      // TODO: Replace the following with your app's Firebase project configuration
-      const firebaseConfig = {
-        // ...
-      };
+    
 
         //Initialise Variables
         //Images must be 640x360
