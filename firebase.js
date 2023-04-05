311d7a2384b9e65d02b64fd1fa79dd0e20f38656
Adam Dunmore <94834543+AdamDunmore@users.noreply.github.com>
2023-04-05 16:54:02 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,18 @@
-      <!-- Insert this script at the bottom of the HTML, but before you use any Firebase services -->
-      import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+<!-- Insert this script at the bottom of the HTML, but before you use any Firebase services -->
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
 
-      // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-      import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js'
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js'
 
-      // Add Firebase products that you want to use
-      import { getAuth } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
-      import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
+// Add Firebase products that you want to use
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
 
-      // TODO: Replace the following with your app's Firebase project configuration
-      const firebaseConfig = {
-        // ...
-      };
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
