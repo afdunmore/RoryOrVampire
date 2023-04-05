cf249a554775d1651f63d9ed27a3feca5e7232c4
Adam Dunmore <94834543+AdamDunmore@users.noreply.github.com>
2023-04-05 16:49:30 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,6 @@
 //Todos:
 // -Add writing for leaderboard
 
-<!-- Insert this script at the bottom of the HTML, but before you use any Firebase services -->
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
-
-// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js'
-
-// Add Firebase products that you want to use
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  // ...
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -26,6 +11,21 @@ function witch(){
 
 class main{
     constructor(){
+      
+      <!-- Insert this script at the bottom of the HTML, but before you use any Firebase services -->
+      import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js'
+
+      // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+      import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js'
+
+      // Add Firebase products that you want to use
+      import { getAuth } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js'
+      import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js'
+
+      // TODO: Replace the following with your app's Firebase project configuration
+      const firebaseConfig = {
+        // ...
+      };
 
         //Initialise Variables
         //Images must be 640x360
